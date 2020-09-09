f6221c8b89d7cec1e7f44ae851510a2363e617cf
HoYoungChun <ho723@naver.com>
2020-09-09 23:43:15 +0900

    Add files via upload

---
+++ EX02/EX02_1.java
@@ -0,0 +1,11 @@
+package cse3040ex02;
+
+public class Ex02_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Wolrd!");
+		System.out.println("Hello, Wolrd!".length());		
+	}
+
+}

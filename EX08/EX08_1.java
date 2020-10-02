abba96a777ac0e2d20fb971609908e9c91d74708
HoYoungChun <ho723@naver.com>
2020-10-03 02:13:12 +0900

    Create EX08_1.java

---
+++ EX08/EX08_1.java
@@ -0,0 +1,28 @@
+package cse3040ex08;
+
+class Employee{
+	private String name;
+	
+	public Employee(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return this.name;
+	}
+}
+
+public class EX08_1 {
+
+	public static void main(String[] args) {
+		Employee e[] = new Employee[5];
+		e[0]=new Employee("Kim");
+		e[1]=new Employee("Lee");
+		e[2]=new Employee("Park");
+		e[3]=new Employee("Choi");
+		e[4]=new Employee("Chung");
+		
+		for(int i = 0; i<5;i++)
+			System.out.println(e[i].getName());
+	}
+
+}

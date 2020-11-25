70033d360fe8a8ccb63a711e9fc1e5b503123b43
HoYoungChun <ho723@naver.com>
2020-11-26 02:39:23 +0900

    Create EX19_6.java

---
+++ EX19/EX19_6.java
@@ -0,0 +1,14 @@
+package cse3040ex19;
+
+import java.util.TreeSet;
+
+public class EX19_6 {
+	public static void main(String[] args) {
+		TreeSet<Integer> set = new TreeSet<Integer>();
+		int[] score = {80, 95, 50, 35, 45, 65, 10, 100};
+		for(int i=0; i<score.length; i++) set.add(Integer.valueOf(score[i]));
+		System.out.println("less than 50: " + set.headSet(Integer.valueOf(50)));
+		System.out.println("greater than or equal to 50: " + set.tailSet(Integer.valueOf(50)));
+
+	}
+}

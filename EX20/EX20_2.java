2fa0b6772b97e2eeedf8a167b99ffe41232ad8b9
HoYoungChun <ho723@naver.com>
2020-11-26 20:52:44 +0900

    Update EX20_2.java

--- EX20/EX20_2.java
+++ EX20/EX20_2.java
@@ -15,24 +15,24 @@ public class EX20_2 {
 		map.put("Lee Java", Integer.valueOf(100));
 		map.put("Kang Java", Integer.valueOf(80));
 		map.put("Ahn Java", Integer.valueOf(90));
-		Set<Map.Entry<String,Integer>> set = map.entrySet();
-		Iterator<Map.Entry<String,Integer>> it = set.iterator();
+		Set<Map.Entry<String, Integer>> set = map.entrySet();
+		Iterator<Map.Entry<String, Integer>> it = set.iterator();
 		while(it.hasNext()) {
-			Map.Entry<String,Integer> e = (Map.Entry<String,Integer>)it.next();
-			System.out.println("name: " + e.getKey() + ", score: " + e.getValue());
+			Map.Entry<String, Integer> e = (Map.Entry<String, Integer>)it.next();
+			System.out.println("name: "+e.getKey()+ ", score: "+e.getValue());
 		}
 		Set<String> keys = map.keySet();
-		System.out.println("participants: " + keys);
+		System.out.println("participants: "+keys);
 		Collection<Integer> values = map.values();
 		Iterator<Integer> it2 = values.iterator();
-		int total = 0;
+		int total=0;
 		while(it2.hasNext()) {
 			Integer i = (Integer)it2.next();
-			total += i.intValue();
+			total+=i.intValue();
 		}
-		System.out.println("total: " + total);
-		System.out.println("average: " + (float)total/set.size());
-		System.out.println("max: " + Collections.max(values));
-		System.out.println("min: " + Collections.min(values));
+		System.out.println("total: "+total);
+		System.out.println("average: "+(float)total/set.size());
+		System.out.println("max: "+Collections.max(values));
+		System.out.println("max: "+Collections.min(values));
 	}
 }

8264c4c39fb843df9de173991d4a20b64652b241
HoYoungChun <ho723@naver.com>
2020-11-29 01:21:15 +0900

    Create EX22_2.java

---
+++ EX22/EX22_2.java
@@ -0,0 +1,31 @@
+package cse3040ex22;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class EX22_2 {
+	public static void main(String[] args) {
+		URL url=null;
+		InputStream in = null;
+		FileOutputStream out = null;
+		String address= "https://icslsogang.github.io/courses/cse3040/sogang_campus.jpg";
+		
+		int ch=0;
+		try {
+			url=new URL(address);
+			in=url.openStream();
+			out=new FileOutputStream("sogang_campus.jpg");
+			
+			while((ch=in.read()) != -1) {
+				out.write(ch);
+			}
+			in.close();
+			out.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("File download complete");
+	}
+}

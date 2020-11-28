2c69f7679dc02ce2b8674d8c8afe66d40ee16dc0
HoYoungChun <ho723@naver.com>
2020-11-29 01:10:32 +0900

    Create EX22_1.java

---
+++ EX22/EX22_1.java
@@ -0,0 +1,27 @@
+package cse3040ex22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class EX22_1 {
+	public static void main(String[] args) {
+		URL url=null;
+		BufferedReader input=null;
+		String address="https://icslsogang.github.io/courses/cse3040/hello.html";
+		String line="";
+		
+		try {
+			url=new URL(address);
+			input = new BufferedReader(new InputStreamReader(url.openStream()));
+			while((line=input.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}

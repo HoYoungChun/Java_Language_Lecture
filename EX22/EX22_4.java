87c1b27c4a03cd003cd264bf70e6f817eaf27496
HoYoungChun <ho723@naver.com>
2020-11-29 01:48:39 +0900

    Create EX22_4.java

---
+++ EX22/EX22_4.java
@@ -0,0 +1,28 @@
+package cse3040ex22;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class EX22_4 {
+	public static void main(String[] args) {
+		String url = "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf";
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Elements bestsellers = doc.select("div.detail");
+		Elements titles = bestsellers.select("div.title");
+		Elements booktitles = titles.select("a[href]");
+		
+		for(int i=0;i<booktitles.size();i++) {
+			System.out.println(i+1+"위: "+booktitles.eq(i).text());
+		}
+	}
+}

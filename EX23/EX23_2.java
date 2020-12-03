19e21785e1dce10f2ddeab3042751620406f778c
HoYoungChun <ho723@naver.com>
2020-12-03 22:24:38 +0900

    Create EX23_2.java

---
+++ EX23/EX23_2.java
@@ -0,0 +1,22 @@
+package cse3040ex23;
+
+class ThreadEx2_1 extends Thread{
+	public void throwException() {
+		try {
+			throw new Exception();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void run() {
+		throwException();
+	}
+}
+
+public class EX23_2 {
+	public static void main(String[] args) {
+		ThreadEx2_1 t1 = new ThreadEx2_1();
+		t1.start();
+	}
+}

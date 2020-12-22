0908fcca626d7decdaa70540d6316cf349a37f11
HoYoungChun <ho723@naver.com>
2020-12-22 13:08:37 +0900

    Add files via upload

---
+++ FP/Client.java
@@ -0,0 +1,107 @@
+package cse3040_fp_20191656;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+class ClientSender extends Thread{
+	Socket socket;
+	DataOutputStream out;
+	DataInputStream in;
+	String ID;
+	ClientSender(Socket socket,String ID){
+		this.socket=socket;
+		try {
+			out=new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(socket.getInputStream());
+			this.ID=ID;
+		}catch(Exception e) { e.printStackTrace();}
+	}
+	
+	public void run() {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			while(true) {
+				System.out.print(ID+">> ");
+				String command= scanner.nextLine();
+				if(command.equals("add")) {
+					System.out.print("add-title> ");
+					String title = scanner.nextLine();
+					if(title.length()==0)
+						continue;
+					System.out.print("add-author> ");
+					String author = scanner.nextLine();
+					if(author.length()==0)
+						continue;
+					
+					out.writeUTF("add");
+					out.writeUTF(title);
+					out.writeUTF(author);
+					
+					System.out.println(in.readUTF());
+				}
+				else if(command.equals("borrow")) {
+					System.out.println("borrow command");
+				}
+				else if(command.equals("return")) {
+					System.out.println("return command");
+				}
+				else if(command.equals("info")) {
+					System.out.println("info command");
+				}
+				else if(command.equals("search")) {
+					System.out.println("search command");
+				}
+				else {
+					System.out.println("[available commands]");
+					System.out.println("add: add a new book to the list of books.");
+					System.out.println("borrow: borrow a book from the library.");
+					System.out.println("return: return a book to the library.");
+					System.out.println("info: show list of books I am currently borrowing.");
+					System.out.println("search: s earch for books.");
+				}
+			}
+		}catch(Exception e) { e.printStackTrace();}
+		
+	}
+}
+
+public class Client {
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			System.out.print("Please give the IP address and port number as arguments.");
+			System.exit(1);
+		}
+		String IpAddress = args[0];
+		int portNum = Integer.parseInt(args[1]);
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			Socket socket = new Socket(IpAddress,portNum);
+			String ID;
+			while(true) {
+				System.out.print("Enter userID>> ");
+				ID= scanner.nextLine();
+				int resist=0;
+				for(int i=0;i<ID.length();i++) {
+					if(!((ID.charAt(i)>='0'&&ID.charAt(i)<='9')||(ID.charAt(i)>='a'&&ID.charAt(i)<='z'))) {
+						resist=1;
+					}
+				}
+				if(resist==0 && ID.length()!=0) {
+					break;
+				}
+				else {
+					System.out.println("UserID must be a single word with lowercase alphabets and numbers");
+				}
+			}
+			System.out.println("Hello "+ID+"!");
+			Thread sender = new Thread(new ClientSender(socket,ID));
+			sender.start();
+		}catch(Exception e) {
+			System.out.print("Connection establishment failed.");
+		}
+		
+	}
+}
---
+++ FP/Server.java
@@ -0,0 +1,193 @@
+package cse3040_fp_20191656;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+class author_borrower{
+	public String author;
+	public String borrower;
+	author_borrower(String a, String b){
+		author=a;
+		borrower=b;
+	}
+}
+
+class MapManager{
+	static public Map<String, author_borrower> map = new hashMap<>();
+	static public void readData(){
+		try {
+	         File file = new File("books.txt");
+	         Scanner scanner = new Scanner(file);
+	        while (scanner.hasNext()) {
+	        	String btitle="";
+	        	String s= scanner.nextLine();
+	        	int i=0;
+	        	while(s.charAt(i)!='\t') {
+	        		btitle+=s.charAt(i);
+	        		i++;
+	        	}
+	        	String intitle = btitle;//title입력받기
+	        	
+	        	i++;
+	        	btitle="";
+	        	while(s.charAt(i)!='\t') {
+	        		btitle+=s.charAt(i);
+	        		i++;
+	        	}
+	        	String inauthor = btitle;//author입력받기
+	        	
+	        	i++;
+	        	btitle="";
+	        	while(i<s.length()) {
+	        		btitle+=s.charAt(i);
+	        		i++;
+	        	}
+	        	String borrower = btitle;//borrower입력받기
+	        	author_borrower ab = new author_borrower(inauthor,borrower);
+	        	
+	        	map.put(intitle,ab);
+	        	
+	         }
+	       } catch (Exception e) { e.printStackTrace();}
+	}
+}
+
+class ValueComparator<T> implements Comparator<T> {
+	public int compare(Object o1, Object o2) {
+		if(o1 instanceof Map.Entry && o2 instanceof Map.Entry) {
+			Map.Entry<String,author_borrower> e1 = (Map.Entry<String, author_borrower>)o1;
+			Map.Entry<String,author_borrower> e2 = (Map.Entry<String, author_borrower>)o2;
+			String v1 = e1.getKey().toLowerCase();
+			String v2 = e2.getKey().toLowerCase();
+			
+			if(v1.compareTo(v2)<0)
+				return -1;
+			else
+				return 1;
+		}
+		return -1;
+	}
+}
+
+class hashMap<String, author_borrower> extends HashMap<String, author_borrower>{
+	public java.lang.String str1="";
+	public java.lang.String toString() {
+		Set<Map.Entry<String,author_borrower>> set = this.entrySet();
+		List<Map.Entry<String,author_borrower>> list = new ArrayList<>(set);
+		Collections.sort(list, new ValueComparator<Map.Entry<String,author_borrower>>());
+		Iterator<Map.Entry<String,author_borrower>> it = list.iterator();
+		while(it.hasNext()) {
+			Map.Entry<String,author_borrower> e = (Map.Entry<String,author_borrower>)it.next();
+			java.lang.String str2 = (e.getKey()+ "\t" +MapManager.map.get(e.getKey()).author+"\t"+MapManager.map.get(e.getKey()).borrower+"\n");
+			str1 = str1 + str2;
+		}
+		return str1;
+	}
+}
+
+class ServerReceiver extends Thread{
+	Socket socket;
+	DataInputStream in;
+	DataOutputStream out;
+	String ID;
+	
+	ServerReceiver(Socket socket){
+		this.socket=socket;
+		try {
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+		}catch(Exception e) { e.printStackTrace();}
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				System.out.println("Client input waiting");
+				String Command = in.readUTF();
+				if(Command.equals("add")) {
+					String title = in.readUTF();
+					String author = in.readUTF();
+					int resist=0;
+					
+					Set set = MapManager.map.keySet();
+					Iterator iterator = set.iterator();
+					while(iterator.hasNext()){
+						  String key = (String)iterator.next();
+						  if(key.toLowerCase().equals(title.toLowerCase()))
+							  resist=1;
+						}
+					
+		        	if(resist==1) {//같은 타이틀 존재
+		        		System.out.println("중복!");
+		        		out.writeUTF("The book already exists in the list.");
+		        	}
+		        	
+		        	else {//같은타이틀 존재 X
+		        		System.out.println("중복없음!");
+		        		out.writeUTF("A new book added to the list.");
+		        	}
+				}
+				else if(Command.equals("borrow")) {
+					;
+				}
+				else if(Command.equals("return")) {
+					;
+				}
+				else if(Command.equals("info")) {
+					;
+				}
+				else if(Command.equals("search")) {
+					;
+				}
+				
+				
+			}
+		}catch(Exception e) { e.printStackTrace();}
+	}
+	
+}
+
+public class Server{
+	public void start(int PortNum) {
+		ServerSocket serverSocket=null;
+		Socket socket = null;
+		try {
+			serverSocket = new ServerSocket(PortNum);
+			while(true) {
+				socket = serverSocket.accept();
+				System.out.println("Client accept");
+				ServerReceiver thread = new ServerReceiver(socket);
+				thread.start();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.out.print("Please give the port number as an argument.");
+			System.exit(1);
+		}
+		
+		MapManager.readData();
+		System.out.println(MapManager.map);
+		
+		int portNum = Integer.parseInt(args[0]);
+		new Server().start(portNum);	
+	}
+}
+
